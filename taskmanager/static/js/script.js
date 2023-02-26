63bec6f9d6f7ff0a3e853ca0f9dc8cb008c11a12
lpewton <liampewton99@gmail.com>
2023-02-26 19:28:26 +0000

    Create task adder option for user to create tasks

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,3 +2,17 @@ document.addEventListener('DOMContentLoaded', function() {
     var sidenav = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(sidenav);
   });
+
+  /* Date picker*/
+  let datepicker = document.querySelectorAll(".datepicker");
+  M.Datepicker.init(datepicker, {
+      format: "dd mmmm, yyyy",
+      i18n: {done: "Select"}
+  });
+
+  /** Selects */
+  document.addEventListener('DOMContentLoaded', function() {
+    var selects = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(selects);
+  });
+
